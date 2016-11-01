5522f53c14cfff8a388ef21ad07c4f494b547fd6
dottjt <Julius_dott@hotmail.com>
2016-11-01 17:11:45 +1100

    rstrst

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
+const cssnano = require('gulp-cssnano');
 
 
  gulp.task('default', () =>
     gulp.src('thumbnails/*')
         .pipe(imagemin())
         .pipe(gulp.dest('static/img/blog/'))
+);
+
+gulp.task('minifycss', () =>
+    gulp.src('themes/reade/static/css/skeleton.css')
+        .pipe(cssnano())
+        .pipe(gulp.dest('themes/reade/static/css/final.css'))
 );
