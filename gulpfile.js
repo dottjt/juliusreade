897741bdfc8c45789788941812437bd3dcb3f8a1
dottjt <Julius_dott@hotmail.com>
2016-11-05 14:14:43 +1100

    rst

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
+/*
 const cssnano = require('gulp-cssnano');
 const cssinline = require('gulp-inline-style');
+*/
+const inlinesource = require('gulp-inline-source');
 
 
 /* This is split into two types of tasks:
@@ -14,18 +17,26 @@ const cssinline = require('gulp-inline-style');
 /* This is to minify images, not for wercker */
 
  gulp.task('default', () =>
-    gulp.src('thumbnails/*')
+    gulp.src('thumbnails/*.html')
         .pipe(imagemin())
         .pipe(gulp.dest('static/img/blog/'))
 );
 
-/* This is for Wercker */
+  gulp.task('inline', () =>
+    gulp.src('public/*')
+        .pipe(inlinesource())
+        .pipe(gulp.dest('public/'))
+);
 
+
+/* This is for Wercker 
 gulp.task('minic', () =>
     gulp.src('themes/reade/static/css/skeleton.css')
         .pipe(cssnano())
         .pipe(gulp.dest('themes/reade/static/cssReal/'))
 );
+*/
+
 
 /*
 
