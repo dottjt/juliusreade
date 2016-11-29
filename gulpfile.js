434490f3174c8dcdea99c74778ca14c36efbdaeb
dottjt <Julius_dott@hotmail.com>
2016-11-29 20:29:51 +1100

    automated commit

--- gulpfile.js
+++ gulpfile.js
@@ -17,16 +17,18 @@ const inlinesource = require('gulp-inline-source');
 /* This is to minify images, not for wercker */
 
  gulp.task('default', () =>
-    gulp.src('thumbnails/*.html')
+    gulp.src('thumbnails/*.png')
         .pipe(imagemin())
         .pipe(gulp.dest('static/img/blog/'))
 );
 
+/*
   gulp.task('inline', () =>
     gulp.src('public/*')
         .pipe(inlinesource())
         .pipe(gulp.dest('public/'))
 );
+*/
 
 
 /* This is for Wercker 
